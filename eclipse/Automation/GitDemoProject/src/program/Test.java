111177bee7365de5fa4e05f9aa08c6cb5b6435e5
deepak <deepaksel28@gmail.com>
2022-06-08 17:04:47 +0530

    This is the First Project in eclipse

---
+++ eclipse/Automation/GitDemoProject/src/program/Test.java
@@ -0,0 +1,13 @@
+package program;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello  Java");
+		
+		System.out.println(10);
+	}
+
+}
